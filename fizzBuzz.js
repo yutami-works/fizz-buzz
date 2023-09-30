7b2d3f00a7e3b7d9722f3be05645f73913725a66
yutami <sabacan623@gmail.com>
2023-09-30 17:14:01 +0900

    js作成

---
+++ fizzBuzz.js
@@ -0,0 +1,13 @@
+// たぶんこれが一番スタンダードだと思います。
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 5 === 0 && i % 3 === 0) {
+    console.log('FizzBuzz!');
+  } else if (i % 5 === 0) {
+    console.log('Buzz!');
+  } else if (i % 3 === 0) {
+    console.log('Fizz!');
+  } else {
+    console.log(i);
+  }
+}
